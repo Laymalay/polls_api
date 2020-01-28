ce3715c5a70cb89d798ea9d9e615736fa67203e9
Alina Zhukouskaya <laymalay@gmail.com>
2020-01-28 11:26:46 +0300

    update use model

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    about = models.TextField(null=True, max_length=500)
+    about = models.TextField(max_length=500, blank=True)
     avatar_key = models.TextField(null=True, max_length=300)
     avatar = models.TextField(null=True, max_length=300)
     
